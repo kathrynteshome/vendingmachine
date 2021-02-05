2fc35309cf57c1226c36ffacf66e8b670251f93f
Kathryn Calano Teshome <calanok@gmail.com>
2021-02-05 09:20:52 -0500

    msg

--- src/main/java/com/techelevator/Item.java
+++ src/main/java/com/techelevator/Item.java
@@ -1,5 +1,70 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Item {
+
+	private String itemID = "";
+	private String name = "";
+	private String type = "";
+	private double price;
+	private int quantity = 5;
+	
+	public Item() {
+		this.itemID = itemID;
+		this.name = name;
+		this.type = type;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public String[] parseItemFile() {
+		String [] itemDetails = null;
+		File vendingMachineCSV = new File ("vendingmachine.csv"); 
+		
+		try(Scanner opensVendingMachineCSV = new Scanner (vendingMachineCSV)){
+		
+			if (opensVendingMachineCSV.hasNext()) {
+				String thisLine = opensVendingMachineCSV.nextLine();
+				itemDetails = thisLine.split("\\|");
+				
+			}
+		} catch (FileNotFoundException e){
+			System.out.println("FileNotFound");
+		}
+		
+		return itemDetails;
+	}
+	
+	public int getQuantity() {
+		
+		return quantity;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity - 1;
+	}
+	
+	public String getItemID() {
+		
+		itemID = itemDetails[0];
+		
+		return itemID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+
 
 }
--- src/test/java/com/techelevator/PuchaseMenuTest.java
+++ src/test/java/com/techelevator/PuchaseMenuTest.java
@@ -1,5 +1,72 @@
 package com.techelevator;
 
-public class PuchaseMenu {
+import org.junit.Test;
 
+import org.junit.*;
+
+public class PuchaseMenuTest {
+
+	@Test
+	//Feed money loop
+	
+		/* Set sum to zero
+		 * Collect money from each line of user input
+		 * Add money to sum */
+	
+	public void does_loop_add_money() {
+		//Arrange
+		double userInput1 = 1.00;
+		double userInput2 = 5.00;
+		//Assert
+		double sum = userInput1 + userInput2;
+		//Act
+		Assert.assertEquals(6.00, sum);
+	}
+	
+	//Select product number
+	
+	@Test
+	
+	public void check_for_null_item_ID() {
+		//Arrange
+		String wrongItemNumber = "A5";
+		boolean itemID = true;
+		String feedback = "Invalid item number";
+		//Assert
+		if (!itemID) {
+			System.out.println(feedback);
+		}
+		//Act
+		Assert.assertEquals("Invalid item number", feedback);
+	}
+	
+	public void check_if_product_quantity_is_zero() {
+		//Arrange
+		int quantity = 0;
+		String feedback = "Item not available";
+		//Assert
+		if (quantity == 0) {
+			System.out.println(feedback);
+		}
+		//Act
+		Assert.assertEquals("Item not available", feedback);
+	}
+	
+	public void check_if_cash_feed_less_than_price() {
+		//Arrange
+		double cashFeedSum = 2;
+		double itemPrice = 3;
+		String feedback = "Insufficient funds";
+		//Assert
+		if (cashFeedSum < itemPrice) {
+			System.out.println(feedback);
+		}
+		//Act
+		Assert.assertEquals("Insufficient funds", feedback);
+	}
+	//Finish transaction (Thank you, Change amount, Message)
+	//Does it update inventory class
+	//Does it append to file
+	//Returns least amount of change
+	
 }
